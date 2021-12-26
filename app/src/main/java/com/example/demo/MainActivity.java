dadeecf535601a02c964e2bcca55889d43431e1b
JudiciousX <1119833684@qq.com>
2021-12-26 13:50:55 +0800

    Initial commit

---
+++ app/src/main/java/com/example/demo/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class MainActivity extends AppCompatActivity {
+
+    public MainActivity() throws IOException {
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    
+}
